9c83ac84108c944f9c0c8f9daaf6090e7003b72d
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-07-06 18:14:16 +0000

    greater btw two

---
+++ Basic-Advance/02.js
@@ -0,0 +1,9 @@
+// Ask two numbers from user and find out which of those two number is greater.
+
+let val1 = Number(prompt("Enter first number"));
+let val2 = Number(prompt("Enter second value"));
+if (val1 > val2) {
+    alert(`${val1} is greater`)
+} else {
+    alert(`${val2} is greater`)
+}
