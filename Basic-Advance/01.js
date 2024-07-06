1408c14493c4ae0a090739862729bb0b0e605672
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-07-06 18:13:46 +0000

    even-odd

---
+++ Basic-Advance/01.js
@@ -0,0 +1,8 @@
+// Ask number from user and find out that number is Even or Odd.
+
+let num = Number(prompt("Write a number"));
+if (num % 2 == 0) {
+    alert("Number is EVEN")
+} else {
+    alert("Number is ODD")
+}
