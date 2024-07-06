b8d64da3c7a8ec111e268f9e77c19ca831a1d083
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-07-06 18:14:47 +0000

    greatyer into terniary

---
+++ Basic-Advance/03.js
@@ -0,0 +1,13 @@
+// Convert below code into Ternary optr. 
+
+// let val1 = Number(prompt("Enter first number"));
+// let val2 = Number(prompt("Enter second value"));
+// if (val1 > val2) {
+//     alert(`${val1} is greater`)
+// } else {
+//     alert(`${val2} is greater`)
+// }
+
+let val1 = Number(prompt("Enter first number"));
+let val2 = Number(prompt("Enter second value"));
+let greaterNumber = val1>val2 ? alert(`${val1} is greater`) : alert(`${val3} is greater`);
