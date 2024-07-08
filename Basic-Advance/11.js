fea829ed2a7cfdf3f263874b9638cd6fee130852
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-07-08 18:14:50 +0000

    sum of 2 numbers

---
+++ Basic-Advance/11.js
@@ -0,0 +1,7 @@
+// Prompt two numbers from user and store them in variable numA and numB, then declare new var sum and add both number using + operator and alert the value.
+
+let numA = Number(prompt("Enter first number"));
+let numB = Number(prompt("Enter second number"));
+
+let sum = numA + numB;
+ alert(`Sum of first and second number is ${sum}`);
