3810ce6a964c2c01fc68a56836777be88504aba7
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-07-08 18:16:13 +0000

    for : odd/even number

---
+++ Basic-Advance/12.js
@@ -0,0 +1,21 @@
+// Complete the following code to make the output be 0 2 4 6 8 10
+// for (let j = 0; j <= 10; ____); console.log(j);
+
+for (let j = 0; j <= 10; j += 2){
+console.log(j);
+}
+
+
+// Print the odd number from 9 to 1 , using for loop.
+
+for (let i = 9; i >= 1; i -= 2){
+    console.log(i);
+}
+
+// OR
+
+for (let i = 9; i >= 1; i --){
+    if(i % 2 !== 0){
+    console.log(i);
+    }
+}
