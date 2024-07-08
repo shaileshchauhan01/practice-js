547c182a40cbf7c85d16c778414162826222cf1b
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-07-08 18:14:00 +0000

    changing value

---
+++ Basic-Advance/10.js
@@ -0,0 +1,15 @@
+// Declare a variable user and prompt the name from user, display the value using alert.
+// Declare a new var username , asign the user value to it and console both user and username 
+// Declare a var age and isLearning with value 22 and true, in same line. 
+// Change the value of user into John and console both user ans udername.
+
+let user = String(prompt("Enter your name"))
+alert(user);
+
+let username = user;
+console.log(user, username);
+
+let age = 22, isLearning = true;
+
+user = "John";
+console.log(user, username);
