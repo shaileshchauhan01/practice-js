3a454395e09793660dcd0d34dabc2978e67ab47c
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-09-06 14:03:10 +0000

    func

---
+++ Basic-Advance/33Practice.js
@@ -0,0 +1,24 @@
+// 1
+function addNumbers(a,b){               // Function dec  , a,b parameters
+    return a+b;
+}
+addNumbers(10,2);                       // func call , 10,2 (values) are arguments
+
+// 2
+function printName(name){
+    console.log(name)
+}
+printName("Shailesh");
+
+// 3
+function num(number){
+    return number * number;
+}
+num(10);
+
+// 4
+function name(firstName, lastName){
+    return `${firstName} ${lastName}`
+    // return firstName + lastName;
+}
+name("Shailesh", "Chauhan")             // func exec
