d55703aa55d3a21460912679ea4218971b84418a
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-11-25 12:42:27 +0000

    event Listener

---
+++ Basic-Advance/73AddEventListener.js
@@ -0,0 +1,26 @@
+let butn = document.querySelector("button");
+
+// butn.addEventListener("click", function () {
+//     console.log("Button Clicked");
+// });
+
+// butn.addEventListener("click", function () {
+//     console.log("Button Clicked Again");
+// });
+
+// butn.addEventListener("dblclick", function () {
+//     console.log("Double Clicked")
+// });
+
+// butn.addEventListener("mousemove", function () {
+//     console.log("Mouse Moved");
+// });
+
+function handler() {
+    console.log("Clicked");
+};
+
+butn.addEventListener("clikc", handler);
+
+// handler()       : function call
+// handler         : function reference
