bde9046d6af445e942603e6cd066301116f736f2
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-11-26 09:12:31 +0000

    Event Handling

---
+++ Basic-Advance/77HandlingEvent.js
@@ -0,0 +1,128 @@
+// const colors = [
+//     "Red",
+//     "Green",
+//     "Blue",
+//     "Aqua",
+//     "Fuchsia",
+//     "Gray",
+//     "Lime",
+//     "Maroon",
+//     "Navy",
+//     "Olive",
+//     "Purple",
+//     "Silver",
+//     "Teal",
+//     "Black",
+//     "White",
+//     "Yellow",
+//     "AliceBlue",
+//     "AntiqueWhite",
+//     "AquaMarine",
+//     "Beige",
+//     "Coral",
+//     "CornflowerBlue",
+//     "DarkCyan",
+//     "DarkGoldenRod",
+//     "DarkOliveGreen",
+//     "DarkOrange",
+//     "DarkOrchid",
+//     "DarkRed",
+//     "DeepPink",
+//     "DodgerBlue",
+//     "Gold",
+//     "HotPink",
+//     "LightCoral",
+//     "LightSeaGreen",
+//     "LightSkyBlue",
+//     "LightSlateGray",
+//     "MediumSeaGreen",
+//     "MediumSlateBlue",
+//     "SlateBlue",
+//     "Tomato",
+//     "Thistle",
+//     "Turquoise",
+//     "Violet",
+//     "Wheat",
+//     "Zinnwaldite",
+//     "Chartreuse",
+//     "Crimson",
+//     "DeepSkyBlue",
+//     "DimGray",
+//     "FireBrick",
+//     "FloralWhite",
+//     "Gainsboro",
+//     "GhostWhite",
+//     "HoneyDew",
+//     "HotSalmon",
+//     "Khaki",
+//     "Lavender",
+//     "LavenderBlush",
+//     "LemonChiffon",
+//     "LightGoldenRodYellow",
+//     "LightPink",
+//     "LightSteelBlue",
+//     "MediumAquaMarine",
+//     "MediumBlue",
+//     "MediumSpringGreen",
+//     "MediumTurquoise",
+//     "MediumVioletRed",
+//     "MistyRose",
+//     "NavajoWhite",
+//     "OldLace",
+//     "PeachPuff",
+//     "Peru",
+//     "PowderBlue",
+//     "RosyBrown",
+//     "RoyalBlue",
+//     "SaddleBrown",
+//     "Salmon",
+//     "SandyBrown",
+//     "SeaGreen",
+//     "Seashell",
+//     "SkyBlue",
+//     "SlateGray",
+//     "Snow",
+//     "SpringGreen",
+//     "SteelBlue",
+//     "Tan",
+//     "Thistle",
+//     "Turquoise",
+//     "VioletRed",
+//     "Wheat",
+//     "YellowGreen",
+//     "Azure",
+//     "Beige",
+//     "LightYellow",
+//     "MediumOrchid",
+//     "MintCream",
+//     "PapayaWhip",
+//     "PeachPuff",
+//     "PineGreen",
+//     "Plum"
+// ];
+
+
+// let button = document.querySelector(".buttons");
+// let h1 = document.querySelector("h1");
+
+// function handleClick(colorText){
+//     h1.innerText = colorText;
+//         h1.style.backgroundColor = colorText;
+// }
+
+// colors.forEach(color => {
+//     let div = document.createElement("div");
+//     div.classList.add("box");
+//     div.style.backgroundColor = color;
+
+//     div.addEventListener("click", function() {
+//         handleClick(color);
+//     });
+
+//     // div.addEventListener("click", function(){
+//     //    h1.innerText = color;
+//     //     h1.style.backgroundColor = color;
+//     // });
+
+//     button.append(div);
+// });
